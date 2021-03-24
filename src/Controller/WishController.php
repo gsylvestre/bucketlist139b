8a7dd6b5ccf65aad4d66a1d21245e56d83dd4919
Guillaume <gsylvestre@gmail.com>
2021-03-24 09:56:04 +0100

    Correction TP 5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,25 +12,26 @@ class WishController extends AbstractController
     /**
      * @Route("/wishes/", name="wish_list")
      */
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
         //todo: requête à la bdd pour aller chercher tous les wishes
+        $wishes = $wishRepository->findBy(["isPublished" => true], ["dateCreated" => "DESC"], 20);
 
         return $this->render('wish/list.html.twig', [
-
+            "wishes" => $wishes
         ]);
     }
 
     /**
      * @Route("/wishes/{id}", name="wish_detail")
      */
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
-        dump($id);
         //todo: requête à la bdd pour aller chercher les infos de ce wish dont nous avons l'id dans l'URL
+        $wish = $wishRepository->find($id);
 
         return $this->render('wish/detail.html.twig', [
-
+            "wish" => $wish
         ]);
     }
 }
